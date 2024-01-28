b9d1e08cef3a45b5ae9380bb5478a0d8a2eef0b5
Eric Pfahl <eric.pfahl@gmail.com>
2024-01-28 08:23:35 -0500

    update mix for hex

--- mix.exs
+++ mix.exs
@@ -1,30 +1,56 @@
 defmodule Cubit.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :cubit,
       version: "0.1.0",
       elixir: "~> 1.15",
+      description: description(),
+      package: package(),
+      docs: docs(),
+      source_url: "https://github.com/epfahl/cubit",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:decimal, "~> 2.1"},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:ratio, "~> 4.0"}
+    ]
+  end
+
+  defp description() do
+    "Elixir library for working with dimensions, units, and measures."
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Eric Pfahl"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: "https://github.com/epfahl/cubit"
+      }
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "Cubit",
+      extras: ["README.md"]
     ]
   end
 end
